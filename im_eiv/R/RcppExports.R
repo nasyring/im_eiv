e45e418946dbc3d1dfad8049b2df6bc9d4c8f9ae
nasyring <nasyrin@gmail.com>
2022-04-19 17:37:45 -0500

    Update RcppExports.R

--- im_eiv/R/RcppExports.R
+++ im_eiv/R/RcppExports.R
@@ -1,21 +1,3 @@
-
-randsetsMCMC <- function(H,A,rL,dimH,M_samp) {
-    .Call(`impred_randsetsMCMC`, H,A,rL,dimH,M_samp)
-}
-
-randsetspred <- function(S,dimS,nsize,n_i,dimn_i,k,U,Ybar) {
-    .Call(`impred_randsetspred`, S,dimS,nsize,n_i,dimn_i,k,U,Ybar)
-}
-
-
-sigmaSolve <- function(Sampsj, SL, aL, aM, lambdaL) {
-    .Call(`impred_sigmaSolve`, Sampsj, SL, aL, aM, lambdaL)
-}
-
-zeroin <- function(ax, bx, u, v, y, z, f, tol) {
-    .Call(`impred_zeroin`, ax, bx, u, v, y, z, f, tol)
-}
-
-root_function <- function(x, Sampsj, SL, aL, lambdaL) {
-    .Call(`impred_root_function`, x, Sampsj, SL, aL, lambdaL)
+im_eiv_plauscontour <- function(par,stat,del,n,propsd,truebx,truebz){
+    .Call(`im_eiv_plauscontour`, par,stat,del,n,propsd,truebx,truebz)
 }
