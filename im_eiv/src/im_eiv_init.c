8c754602ae1b3954d28117696edb9c39dd133497
nasyring <nasyrin@gmail.com>
2022-04-19 17:36:03 -0500

    Update im_eiv_init.c

--- im_eiv/src/im_eiv_init.c
+++ im_eiv/src/im_eiv_init.c
@@ -4,22 +4,15 @@
 #include <R_ext/Rdynload.h>
 
 /* .Call calls */
-extern SEXP impred_randsetsMCMC(SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP impred_randsetspred(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP impred_sigmaSolve(SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP impred_zeroin(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP impred_root_function(SEXP, SEXP, SEXP, SEXP, SEXP);
+
+extern SEXP im_eiv_plauscontour(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-    {"impred_randsetsMCMC", (DL_FUNC) &impred_randsetsMCMC, 5},
-    {"impred_randsetspred", (DL_FUNC) &impred_randsetspred, 8},
-    {"impred_sigmaSolve", (DL_FUNC) &impred_sigmaSolve, 5}, 
-    {"impred_zeroin", (DL_FUNC) &impred_zeroin, 8},
-    {"impred_root_function", (DL_FUNC) &impred_root_function, 8}, 
+    {"im_eiv_plauscontour", (DL_FUNC) &im_eiv_plauscontour, 7},
     {NULL, NULL, 0}
 };
 
-void R_init_impred(DllInfo *dll)
+void R_init_im_eiv(DllInfo *dll)
 {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
