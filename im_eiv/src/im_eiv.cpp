053c63dbbe5a745dd33793621940ba6589312d1f
nasyring <nasyrin@gmail.com>
2022-04-20 15:47:04 -0500

    Update im_eiv.cpp

--- im_eiv/src/im_eiv.cpp
+++ im_eiv/src/im_eiv.cpp
@@ -186,8 +186,11 @@ Rcpp::List plauscontour(NumericVector par, NumericVector stat, NumericVector del
      				       { dz2dbx[0], dz2dbz[0], dz2dmux[0], dz2dsx[0], dz2dse[0] } };
 	
 				detJ[0] = log(std::abs(arma::det(J)));
-
-				currdens[0]  = detJ[0] + R::dchisq(v1[0]*v1[0],n[0]-1.0,1) +  R::dchisq(v3[0]*v3[0],n[0]-2.0,1) + R::dnorm(v2[0],0.0,1.0,1) + R::dnorm(z1[0],0.0,sd[0],1) + R::dnorm(z2[0],0.0,sd[0],1);
+				if((v1[0]>0) & (v3[0]>0)){
+					currdens[0]  = detJ[0] + R::dchisq(v1[0]*v1[0],n[0]-1.0,1) +  R::dchisq(v3[0]*v3[0],n[0]-2.0,1) + R::dnorm(v2[0],0.0,1.0,1) + R::dnorm(z1[0],0.0,sd[0],1) + R::dnorm(z2[0],0.0,sd[0],1);
+				}else {
+					currdens[0]  = -1000000;
+				}
 		}
 			if(currdens[0] == -99.0){
 				uu[0] = 1.0;
