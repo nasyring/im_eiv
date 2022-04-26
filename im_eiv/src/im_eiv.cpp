1df45e2320c57673c4679694e12aa7e3094ddec6
nasyring <nasyrin@gmail.com>
2022-04-25 20:23:46 -0500

    Update im_eiv.cpp

--- im_eiv/src/im_eiv.cpp
+++ im_eiv/src/im_eiv.cpp
@@ -450,10 +450,6 @@ Rcpp::List plauscontourIMmarg(NumericVector stat, NumericVector del, NumericVect
 	std::sort(sampslo.begin(), sampslo.end());
 	std::sort(sampshi.begin(), sampshi.end());
 
-	result = Rcpp::List::create(Rcpp::Named("sampshi") = sampshi, Rcpp::Named("sampslo") = sampslo);
-	
-	return result;
-	/*
 	// Computing plausibility contour of beta_x using grid of variance components and MC density random set
 	
 	NumericVector uni(1,0.0);
@@ -487,7 +483,7 @@ Rcpp::List plauscontourIMmarg(NumericVector stat, NumericVector del, NumericVect
 	result = Rcpp::List::create(Rcpp::Named("plaus_beta_x") = plaustruebetax, Rcpp::Named("plauses_beta_x") = plausbetax);
 	
 	return result;
-	*/
+	
 }
 	
 
