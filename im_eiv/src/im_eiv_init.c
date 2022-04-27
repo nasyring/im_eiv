0f14bc72a8cb93618bef2c3c20cfed2862adfc4b
nasyring <nasyrin@gmail.com>
2022-04-27 11:00:45 -0500

    Update im_eiv_init.c

--- im_eiv/src/im_eiv_init.c
+++ im_eiv/src/im_eiv_init.c
@@ -5,13 +5,13 @@
 
 /* .Call calls */
 
-extern SEXP imeiv_plauscontourGF(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP imeiv_plauscontourGF(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP imeiv_plauscontourIM(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP imeiv_plauscontourIMmarg(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP imeiv_sortmat(SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-    {"imeiv_plauscontourGF", (DL_FUNC) &imeiv_plauscontourGF, 7},
+    {"imeiv_plauscontourGF", (DL_FUNC) &imeiv_plauscontourGF, 9},
     {"imeiv_plauscontourIM", (DL_FUNC) &imeiv_plauscontourIM, 9},
     {"imeiv_plauscontourIMmarg", (DL_FUNC) &imeiv_plauscontourIMmarg, 7},
     {"imeiv_sortmat", (DL_FUNC) &imeiv_sortmat, 2},
