35352fbfd5c05ad62d1dce6c4f058e84dacd4e87
nasyring <nasyrin@gmail.com>
2022-05-06 17:50:47 -0500

    Update RcppExports.R

--- im_eiv/R/RcppExports.R
+++ im_eiv/R/RcppExports.R
@@ -1,29 +1,9 @@
-plauscontourGF <- function(par,stat,del,type,n,propsd,truebx,truebz,bxseq,bzseq){
-    .Call(`imeiv_plauscontourGF`, par,stat,del,type,n,propsd,truebx,truebz,bxseq,bzseq)
-}
-
-plauscontourGFu <- function(par,stat,del,type,n,propsd,truebx,truebz,bxseq,bzseq){
-    .Call(`imeiv_plauscontourGFu`, par,stat,del,type,n,propsd,truebx,truebz,bxseq,bzseq)
-}
-
-plauscontourGFv <- function(par,stat,del,type,n,propsd,truebx,bxseq,randsettype){
-    .Call(`imeiv_plauscontourGFv`, par,stat,del,type,n,propsd,truebx,bxseq,randsettype)
+plauscontourMCMC <- function(par,stat,del,type,n,propsd,truebx,bxseq,truebz,bzseq,randsettype){
+    .Call(`imeiv_plauscontourMCMC`, par,stat,del,type,n,propsd,truebx,bxseq,truebz,bzseq,randsettype)
 }
 
 plauscontourMC <- function(sampsize,stat,del,type,n,truebx,bxseq,truebz,bzseq){
     .Call(`imeiv_plauscontourMC`, sampsize,stat,del,type,n,truebx,bxseq,truebz,bzseq)
-}
-
-plauscontourGF2 <- function(par,stat,del,n,propsd,truebx,truebz,bxseq,bzseq){
-    .Call(`imeiv_plauscontourGF2`, par,stat,del,n,propsd,truebx,truebz,bxseq,bzseq)
-}
-
-plauscontourIM <- function(stat,del,type,n,truebx,truebz,bxseq,sxseq,seseq){
-    .Call(`imeiv_plauscontourIM`, stat,del,type,n,truebx,truebz,bxseq,sxseq,seseq)
-}
-
-plauscontourIMmarg <- function(stat,del,type,n,truebx,truebz,bxseq){
-    .Call(`imeiv_plauscontourIMmarg`, stat,del,type,n,truebx,truebz,bxseq)
 }
 
 sortmat <- function(x,col){
