1fbded760fa45751b44439a98c4114a9aee6db86
nasyring <nasyrin@gmail.com>
2022-05-11 17:44:50 -0500

    Update RcppExports.R

--- im_eiv/R/RcppExports.R
+++ im_eiv/R/RcppExports.R
@@ -6,6 +6,14 @@ plauscontourMC <- function(sampsize,stat,del,type,n,truebx,bxseq,truebz,bzseq,ra
     .Call(`imeiv_plauscontourMC`, sampsize,stat,del,type,n,truebx,bxseq,truebz,bzseq,randsetpred)
 }
 
+plauscontourMC2 <- function(sampsize,stat,del,type,n,truebx,bxseq,truebz,bzseq,randsetpred){
+    .Call(`imeiv_plauscontourMC2`, sampsize,stat,del,type,n,truebx,bxseq,truebz,bzseq,randsetpred)
+}
+
 sortmat <- function(x,col){
     .Call(`imeiv_sortmat`, x,col)
+}
+
+grow <- function(x){
+    .Call(`imeiv_grow`, x)
 }
