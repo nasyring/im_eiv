d6478821952909e8684eccd95f2aae7aa1d6959b
nasyring <nasyrin@gmail.com>
2022-05-13 13:44:36 -0500

    Update RcppExports.R

--- im_eiv/R/RcppExports.R
+++ im_eiv/R/RcppExports.R
@@ -2,6 +2,10 @@ plauscontourMCMC <- function(par,stat,del,type,n,propsd,truebx,bxseq,truebz,bzse
     .Call(`imeiv_plauscontourMCMC`, par,stat,del,type,n,propsd,truebx,bxseq,truebz,bzseq,randsettype)
 }
 
+plauscontourMCMC2 <- function(par,stat,del,type,n,propsd,truebx,bxseq,truebz,bzseq,randsettype){
+    .Call(`imeiv_plauscontourMCMC2`, par,stat,del,type,n,propsd,truebx,bxseq,truebz,bzseq,randsettype)
+}
+
 plauscontourMC <- function(sampsize,stat,del,type,n,truebx,bxseq,truebz,bzseq,randsetpred){
     .Call(`imeiv_plauscontourMC`, sampsize,stat,del,type,n,truebx,bxseq,truebz,bzseq,randsetpred)
 }
