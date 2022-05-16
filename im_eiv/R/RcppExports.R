4372473eec23aab5f2c0569759a2469ec381b796
nasyring <nasyrin@gmail.com>
2022-05-16 12:56:21 -0500

    Update RcppExports.R

--- im_eiv/R/RcppExports.R
+++ im_eiv/R/RcppExports.R
@@ -2,6 +2,11 @@ plauscontourMCMC <- function(par,stat,del,type,n,propsd,truebx,bxseq,truebz,bzse
     .Call(`imeiv_plauscontourMCMC`, par,stat,del,type,n,propsd,truebx,bxseq,truebz,bzseq,randsettype)
 }
 
+plauscontourMCMCcond <- function(sampsize ,  stat ,   del ,  propsd , type , n , truebx , truebz , bxseq , sxseq , lenseq , plbxseq , plbzseq , lenplseq , se2){
+    .Call(`imeiv_plauscontourMCMCcond`, sampsize ,  stat ,   del ,  propsd , type , n , truebx , truebz , bxseq , sxseq , lenseq , plbxseq , plbzseq , lenplseq , se2)
+}
+ 
+
 plauscontourMCMC2 <- function(par,stat,del,type,n,propsd,truebx,bxseq,truebz,bzseq,randsettype){
     .Call(`imeiv_plauscontourMCMC2`, par,stat,del,type,n,propsd,truebx,bxseq,truebz,bzseq,randsettype)
 }
