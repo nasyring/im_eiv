bb37ae30a6d37b475fd4e20e6931f50262721293
nasyring <nasyrin@gmail.com>
2022-05-29 17:28:24 -0500

    Update im_eiv.cpp

--- im_eiv/src/im_eiv.cpp
+++ im_eiv/src/im_eiv.cpp
@@ -1507,10 +1507,12 @@ Rcpp::List plauscontourMC2(NumericVector sampsize, NumericVector stat, NumericVe
 	NumericVector plausesz(500,0.0);
 	NumericVector plausestruz(1,0.0);
 	
-	int inc = round(floor(size / 500));
+	NumericVector inc(1, 0.0); inc[0] = (bx_s[size-1]-bx_s[0])/500.0;
+	NumericVector inc2(1, 0.0); inc2[0] = (bz_s[size-1]-bz_s[0])/500.0;
 	NumericVector bx_seq(500, 0.0);NumericVector bz_seq(500, 0.0);
-	for(int i = 0; i < 500; i++){
-		bx_seq[i] = bx_s[i*inc];bz_seq[i] = bz_s[i*inc];	
+	bx_seq[0] = bx_s[0];
+	for(int i = 1; i < 500; i++){
+		bx_seq[i] = bx_s[i-1]+inc[0]; bz_seq[i] = bz_s[i-1]+inc2[0];	
 	}
 	bx_seq[499] = bx_s[(size-1)];bz_seq[499] = bz_s[(size-1)];	
 	
