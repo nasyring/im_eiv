5717a942937247f99803cd054903bb888feb6e7c
nasyring <nasyrin@gmail.com>
2022-05-29 10:00:36 -0500

    Update RcppExports.cpp

--- im_eiv/src/RcppExports.cpp
+++ im_eiv/src/RcppExports.cpp
@@ -50,8 +50,8 @@ BEGIN_RCPP
 END_RCPP
 }
 
-Rcpp::List plauscontourSIR(NumericVector sampsize, NumericVector stat, NumericVector del, NumericVector n, NumericVector mode, NumericVector local_pt, NumericVector se2, NumericVector cond_par);
-RcppExport SEXP imeiv_plauscontourSIR(SEXP sampsizeSEXP, SEXP statSEXP, SEXP delSEXP, SEXP nSEXP, SEXP modeSEXP, SEXP local_ptSEXP, SEXP se2SEXP, SEXP cond_parSEXP){
+Rcpp::List plauscontourSIR(NumericVector sampsize, NumericVector stat, NumericVector del, NumericVector n, NumericVector mode, NumericVector dens, NumericVector se2, NumericVector cond_par);
+RcppExport SEXP imeiv_plauscontourSIR(SEXP sampsizeSEXP, SEXP statSEXP, SEXP delSEXP, SEXP nSEXP, SEXP modeSEXP, SEXP densSEXP, SEXP se2SEXP, SEXP cond_parSEXP){
 BEGIN_RCPP
     Rcpp::RObject __result;
     Rcpp::RNGScope __rngScope;
@@ -60,10 +60,10 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< NumericVector >::type del(delSEXP);
     Rcpp::traits::input_parameter< NumericVector >::type n(nSEXP);
     Rcpp::traits::input_parameter< NumericVector >::type mode(modeSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type local_pt(local_ptSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type dens(densSEXP);
     Rcpp::traits::input_parameter< NumericVector >::type se2(se2SEXP);  
     Rcpp::traits::input_parameter< NumericVector >::type cond_par(cond_parSEXP);
-    __result = Rcpp::wrap(plauscontourSIR( sampsize ,  stat ,   del  , n , mode , local_pt, se2, cond_par ));
+    __result = Rcpp::wrap(plauscontourSIR( sampsize ,  stat ,   del  , n , mode , dens, se2, cond_par ));
     return __result;
 END_RCPP
 }
