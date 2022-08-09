f3544eefe7e22048f8785d45e49b04b095755471
nasyring <nasyrin@gmail.com>
2022-08-09 10:05:29 -0500

    Update RcppExports.cpp

--- im_eiv/src/RcppExports.cpp
+++ im_eiv/src/RcppExports.cpp
@@ -7,18 +7,20 @@ using namespace std;
 
 
 
-Rcpp::List plausMC(NumericVector theta, NumericMatrix grid, NumericVector stat, NumericVector del, NumericVector df, int m_samps);
+Rcpp::List plausMC(NumericVector theta, NumericVector intcpt, NumericMatrix grid, NumericVector stat, NumericVector del, NumericVector df, int m_samps);
 RcppExport SEXP imeiv_plausMC(SEXP thetaSEXP, SEXP gridSEXP, SEXP statSEXP, SEXP delSEXP, SEXP dfSEXP, SEXP m_sampsSEXP){
 BEGIN_RCPP
     Rcpp::RObject __result;
     Rcpp::RNGScope __rngScope;
     Rcpp::traits::input_parameter< NumericVector >::type theta(thetaSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type intcpt(intcptSEXP);
     Rcpp::traits::input_parameter< NumericMatrix >::type grid(gridSEXP);
     Rcpp::traits::input_parameter< NumericVector >::type stat(statSEXP);
     Rcpp::traits::input_parameter< NumericVector >::type del(delSEXP);
     Rcpp::traits::input_parameter< NumericVector >::type df(dfSEXP);
     Rcpp::traits::input_parameter< int >::type m_samps(m_sampsSEXP);
-    __result = Rcpp::wrap(plausMC(theta, grid, stat, del, df, m_samps));
+    Rcpp::traits::input_parameter< bool >::type intercept(interceptSEXP);
+    __result = Rcpp::wrap(plausMC(theta, intcpt, grid, stat, del, df, m_samps, intercept));
     return __result;
 END_RCPP
 }    
